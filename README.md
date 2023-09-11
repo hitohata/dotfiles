36b1307714ec45a36b50029cfceef72a5570a6e3
hirohata <43802663+hitohata@users.noreply.github.com>
2023-09-11 16:57:24 -0700

    add Bashrc (#1)

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# dotfiles
+# My Dot Files
+
+## Require
+
+create a ~/.envfiles dir and clone in there.
+
+## Install
+
+run following:
+
+```bash
+source installer.sh
+```
+
+This script creates a .DotenvBackups directory and moves .env files to there.
+And adds symbolic links.
---
+++ installer.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+# create backup directory
+
+BACKUP_DIR="${HOME}/.DotenvBackups"
+
+if [ ! -d $BACKUP_DIR ];
+then
+    mkdir -p $BACKUP_DIR
+fi
+
+# .bashrc
+## remove .bashrc and add link
+if [ -f ${HOME}/.bashrc ];
+then
+    mv ${HOME}/.bashrc ${BACKUP_DIR}/.bashrc
+    ln -s ${HOME}/.dotfiles/.bashrc ~
+fi
