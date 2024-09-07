c1bdb1c79d95bc5c71134322cb4bf28a35d91de4
hirohata <hirohatatro@gmail.com>
2024-09-07 10:41:57 -0700

    added rust settings

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -1,4 +1,10 @@
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = "unnamed"
+vim.opt.shiftwidth = 2
+vim.opt.wrap = true
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true
+
+vim.wo.relativenumber=true
 vim.opt.shiftwidth = 2
 vim.opt.wrap = true
 vim.opt.spelllang = "en_us"
