74dcd407d86d28a545ff48ccc07adf3504cc1822
hirohata <43802663+hitohata@users.noreply.github.com>
2024-09-19 13:30:16 -0700

    Ts lsp (#26)
    * added rust settings
    
    * setuped lsps

--- nvchad/custom/configs/lint.lua
+++ nvchad/custom/configs/lint.lua
@@ -1,8 +1,3 @@
-require("lint").linters_by_ft = {
-  javascript = {'eslint'},
-  typescript = {'eslint'}
-}
-
 vim.api.nvim_create_autocmd({"BufWritePost"}, {
   callback = function ()
     require("lint").try_lint()
